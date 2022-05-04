33114cc2800defc37fe7b5d1329ab587395d5def
mjbernal <mjbernal2000@gmail.com>
2022-05-04 16:38:39 +0200

    Prueba modelo R 2

--- mymodel-R/MyModel.R
+++ mymodel-R/MyModel.R
@@ -4,13 +4,13 @@ library(mlbench)
 
 predict.mymodel <- function(mymodel,newdata=list()) {
 
+  # Extraer el individuo a consultar
   individuo<-newdata[[1]][1]
 
   # Cargar los datos y hacer la partición
   data("PimaIndiansDiabetes") 
   pima.Datos = PimaIndiansDiabetes
   pima.Var.Salida.Usada = c("diabetes")
-  pima.Vars.Entrada.Usadas = setdiff(names(pima.Datos),pima.Var.Salida.Usada)
   set.seed(1234)
   pima.TrainIdx.80<-createDataPartition(pima.Datos[[pima.Var.Salida.Usada]],p=0.8,list = FALSE,times = 1)
   pima.Datos.Train<-pima.Datos[pima.TrainIdx.80,]

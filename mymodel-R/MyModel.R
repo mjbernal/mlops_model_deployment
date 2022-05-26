dc422fd306fd92f21f47e7365b43bcfa0576264f
mjbernal <mjbernal2000@gmail.com>
2022-05-27 00:33:25 +0200

    Prueba

--- mymodel-R/MyModel.R
+++ mymodel-R/MyModel.R
@@ -16,7 +16,7 @@ predict.mymodel <- function(mymodel,newdata=list()) {
   pima.Datos.Train<-pima.Datos[pima.TrainIdx.80,]
 
   # Predict
-  cat(predict(mymodel$model,pima.Datos.Train[individuo,], "\n")
+  cat(predict(mymodel$model,pima.Datos.Train[individuo,]), "\n")
 }
 
 
